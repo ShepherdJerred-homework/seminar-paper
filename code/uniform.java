09041645b529cefce72b205d6f286aa17dabd804
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-04-03 00:55:44 -0500

    Add matrix and indexing. Strengthen conclusion.

--- code/uniform.java
+++ code/uniform.java
@@ -1,5 +1,10 @@
-var matrix = new Matrix4f();
+// Create a new perspective matrix
+var matrix = new Matrix4f().perspective(FIELD_OF_VIEW, aspectRatio, Z_NEAR, Z_FAR);
+
+// Get the name of the matrix
 glUniformName = glGetUniformLocation(glShaderProgramName, "projectionMatrix");
+
+// Move the matrix to native memory, and then buffer it
 try (MemoryStack stack = MemoryStack.stackPush()) {
   FloatBuffer fb = stack.mallocFloat(16);
   matrix.get(fb);
